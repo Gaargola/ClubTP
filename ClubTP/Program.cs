bb4505ca051ee73bfbdfd18df99c856a0c3d33b7
Mauro Borda <tl3pr0yt@gmail.com>
2025-06-19 20:11:25 -0300

    primer commit

---
+++ ClubTP/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: demondev
+ * Date: 19/6/2025
+ * Time: 19:59
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ClubTP
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
