5760790e748791820187085fe0ef23f1a86241d5
nicodyj1 <nicodefe23@gmail.com>
2025-06-19 21:18:55 -0300

    cree clase persona

---
+++ ClubTP/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClubTP
+{
+	/// <summary>
+	/// Description of Persona.
+	/// </summary>
+	public class Persona
+	{
+		private string Nombre;
+		private int Dni;
+		private int Edad;
+		
+		public Persona(string Nombre, int Dni, int Edad)
+		{
+			this.Nombre = Nombre;
+			this.Dni = Dni;
+			this.Edad = Edad;
+		}
+		public definirNombre(){
+			Console.WriteLine("Ingrese nombre de la persona: ");
+		
+		}
+	}
+}
+
