f0c91160a8f3401d62f088383fe16acea5fedd74
nicodyj1 <nicodefe23@gmail.com>
2025-06-19 21:39:13 -0300

    termine clase persona, comence clase entrenador

---
+++ ClubTP/Entrenador.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: nico user
+ * Date: 19/6/2025
+ * Time: 21:38
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ClubTP
+{
+	/// <summary>
+	/// Description of Entrenador.
+	/// </summary>
+	public class Entrenador
+	{
+		public Entrenador()
+		{
+		}
+	}
+}
--- ClubTP/Program.cs
+++ ClubTP/Program.cs
@@ -14,10 +14,11 @@ namespace ClubTP
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
 			
-			// TODO: Implement Functionality Here
-			
+			Persona a = new Persona();
+			a.mostrarNombre();
+			a.definirNombre();
+			a.mostrarNombre();
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
