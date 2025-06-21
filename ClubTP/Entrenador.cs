cac091b3dc7053d330174b480662c28b62c2dc79
Mauro Borda <tl3pr0yt@gmail.com>
2025-06-21 03:54:24 -0300

    Correciones

--- ClubTP/Entrenador.cs
+++ ClubTP/Entrenador.cs
@@ -4,28 +4,34 @@ namespace ClubTP
 {
 	public class Entrenador: Persona
 	{
-		private int Legajo;
+		private int legajo;
 		
 		public Entrenador(){}
 		public Entrenador(int legajo)
 		{
-			this.Legajo = legajo;
+			this.legajo = legajo;
 		}
 
-    // Metodos
-		public int definirLegajo(){
-			Console.WriteLine("Ingrese el Legajo del entrenador: ");
-			this.Legajo = int.Parse(Console.ReadLine());
-			return Legajo;
-		}
+		//public int definirLegajo(){
+		//	Console.WriteLine("Ingrese el Legajo del entrenador: ");
+		//	this.Legajo = int.Parse(Console.ReadLine());
+		//	return Legajo;
+		//}
+    
+    // Propiedades
+    public int Legajo
+    {
+      get {return legajo;}
+      set {legajo = value;}
+    }
+    
+    // Métodos
 		public int retornarLegajo()
 		{
 			return Legajo;
 		}
 		public void mostrarLegajo(){
-			Console.WriteLine("El legajo del entrenador es "+Legajo);
+			Console.WriteLine("El legajo del entrenador es {0}",legajo);
     }
-
-    // Vamos a agregar propiets?
 	}
 }
